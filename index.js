66dee004e7d3744b9131a82aa0e3509e3f2bbeb3
7002bejon <114094974+7002bejon@users.noreply.github.com>
2024-01-10 15:01:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ function changeVariables() {
     } else if (document.getElementById("3").checked) {
         numDice = 3;
     }
-    let input = document.getElementById("frequencyInput").value;
-    if (Number.isInteger(input)) {
-        frequency = input;
-    }
-    console.log(frequency);
+    let input = document.getElementById("frequencyInput");
+    frequency = input.value;
 }
+//Works
+function getRandom() {
+    return 1 + Math.floor(Math.random() * 6);
+}
+//Works
+function rollDice() {
+    let individualResults = [];
+    let finalResults = [];
+    for (i = 0; i < frequency; i++) {
+        let final = 0;
+        for (j = 0; j < numDice; j++) {
+         let roll = getRandom();
+         final+= roll;
+         individualResults.push(roll);
+        }
+        finalResults.push(final);
+    }
+    console.log(individualResults);
+    console.log(finalResults);
+}
+
