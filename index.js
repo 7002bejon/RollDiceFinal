a6e3bbff6aec00e4364beb070935a6c2655a006d
7002bejon <114094974+7002bejon@users.noreply.github.com>
2024-01-12 15:25:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,3 +33,53 @@ function rollDice() {
     console.log(finalResults);
 }
 
+function makeTable() {
+    let table = document.getElementById("tableBody");
+
+    let Headrow = document.createElement("tr");
+
+    let frequencyHead = document.createElement("th");
+    frequencyHead.innerText = "Frequency";
+    frequencyHead.colSpan = 2;
+    Headrow.appendChild(frequencyHead);
+    table.appendChild(Headrow);
+    if (numDice == 2) {
+    let doublesHead = document.createElement("th");
+    doublesHead.innerText = "Doubles Rolled";
+    Headrow.appendChild(doublesHead);
+    table.appendChild(Headrow);
+    } else if (numDice == 3) {
+        let triplesHead = document.createElement("th");
+        triplesHead.innerText = "Triples Rolled";
+        Headrow.appendChild(triplesHead);
+        table.appendChild(Headrow);
+    }
+    let meanHead = document.createElement("th");
+    meanHead.innerText = "Mean";
+    Headrow.appendChild(meanHead);
+    table.appendChild(Headrow);
+    let medianHead = document.createElement("th");
+    medianHead.innerText = "Median";
+    Headrow.appendChild(medianHead);
+    table.appendChild(Headrow);
+    let modeHead = document.createElement("th");
+    modeHead.innerText = "Mode";
+    Headrow.appendChild(modeHead);
+    table.appendChild(Headrow);
+    
+    if (numDice == 1) {
+      for (i = 1; i < 7; i++) {
+    let row = table.insertRow(-1);
+    let c1 = row.insertCell(0);
+    let c2 = row.insertCell(1);
+    c1.innerHTML = i;
+    let count = 0;
+    for (j = 0; j < finalResults.length; j++) {
+        if (finalResults[j] == i) {
+            count++;
+        }
+    }
+    c2.innerHTML = count;
+    }
+}
+}
