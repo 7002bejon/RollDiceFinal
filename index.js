93dd00e4415358da479355728ee37685e23ac1c9
7002bejon <114094974+7002bejon@users.noreply.github.com>
2024-01-22 15:25:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -72,6 +72,35 @@ function makeTable() {
     table.appendChild(Headrow);
     
     if (numDice == 1) {
+        //Mean
+        let mean = 0;
+        let counter = 0;
+        let final = 0;
+        for (i = 0; i < individualResults.length; i++) {
+          counter++
+        }
+        for (i = 0; i < finalResults.length; i++) {
+            final+=finalResults[i];
+        }
+        mean = Math.round((final / counter) * 100) / 100;
+        //Median
+        let counting = counter / 2
+        let median = individualResults[counting];
+        //Mode
+        let mostCommon = individualResults[0];
+        let countm = 0;
+        for (i = 0; i < counter; i++) {
+        let count = 0;
+            for (j = 1; j < counter; j++) {
+              if (individualResults[i] == individualResults[j]) {
+                count++;
+              }
+            }
+            if (count > countm ) {
+                countm = count
+                mostCommon = individualResults[i];
+            }
+        }
       for (i = 1; i < 7; i++) {
     let row = table.insertRow(-1);
     let c1 = row.insertCell(0);
@@ -84,20 +113,51 @@ function makeTable() {
             console.log("work");
         }
     }
+    let c3 = row.insertCell(2);
+    c3.innerHTML = mean;
+    let c4 = row.insertCell(3);
+    c4.innerHTML = median;
     c2.innerHTML = count;
+    let c5 = row.insertCell(4);
+    c5.innerHTML = mostCommon;
     }
 }
 // 2 Dice
+// Doubles Rolled
 if (numDice == 2) {
+    let mean = 0;
+    let counter = 0;
+    let final = 0;
+    for (i = 0; i < individualResults.length; i++) {
+      counter++
+    }
+    for (i = 0; i < finalResults.length; i++) {
+        final+=finalResults[i];
+    }
+    mean = Math.round((final / counter) * 100) / 100;
+    //Median
+    let counting = counter / 2
+    let median = individualResults[counting];
+    //Mode
+    let mostCommon = individualResults[0];
+    let countm = 0;
+    for (i = 0; i < counter; i++) {
+    let count = 0;
+        for (j = 1; j < counter; j++) {
+          if (individualResults[i] == individualResults[j]) {
+            count++;
+          }
+        }
+        if (count > countm ) {
+            countm = count
+            mostCommon = individualResults[i];
+        }
+    }
     let doublesRolled = 0;
-    for (let k=0; k < individualResults.length; k++) {
-        for (let l=1; l < individualResults.length; l++) {
-            console.log(individualResults[k]);
-            console.log(individualResults[l]);
-            if (individualResults[k] == individualResults[l] && k != l) {
-                doublesRolled++;
-                console.log("yo");
-            }
+    for (let k=0; k < individualResults.length; k+=2) {
+        if (individualResults[k] == individualResults[k + 1]) {
+            doublesRolled++;
+            console.log("yo");
         }
       }
     //Frequency
@@ -108,6 +168,12 @@ if (numDice == 2) {
   if (i == 2) {
     let c3 = row.insertCell(2);
     c3.innerHTML = doublesRolled;
+    let c4 = row.insertCell(3);
+  c4.innerHTML = mean;
+  let c5 = row.insertCell(4);
+    c5.innerHTML = median;
+    let c6 = row.insertCell(5);
+    c6.innerHTML = mostCommon;
   }
   c1.innerHTML = i;
   let count = 0;
@@ -118,16 +184,62 @@ if (numDice == 2) {
       }
   }
   c2.innerHTML = count;
+
   }
-  //DoublesRolled
 }
 
 //3 Dice
 if (numDice == 3) {
+    let mean = 0;
+    let counter = 0;
+    let final = 0;
+    for (i = 0; i < individualResults.length; i++) {
+      counter++
+    }
+    for (i = 0; i < finalResults.length; i++) {
+        final+=finalResults[i];
+    }
+    mean = Math.round((final / counter) * 100) / 100;
+    //Median
+    let counting = counter / 2
+    let median = individualResults[counting];
+     //Mode
+     let mostCommon = individualResults[0];
+     let countm = 0;
+     for (i = 0; i < counter; i++) {
+     let count = 0;
+         for (j = 1; j < counter; j++) {
+           if (individualResults[i] == individualResults[j]) {
+             count++;
+           }
+         }
+         if (count > countm ) {
+             countm = count
+             mostCommon = individualResults[i];
+         }
+     }
+    let triplesRolled = 0;
+    for (let k=0; k < individualResults.length; k+=3) {
+        if (individualResults[k] == individualResults[k + 1] && individualResults[k + 1] == individualResults[k + 2]) {
+            triplesRolled++;
+            console.log("yo");
+        }
+      }
+      //Frequency
     for (i = 3; i < 19; i++) {
   let row = table.insertRow(-1);
   let c1 = row.insertCell(0);
   let c2 = row.insertCell(1);
+  if (i == 3) {
+    let c3 = row.insertCell(2);
+    c3.innerHTML = triplesRolled;
+    let c4 = row.insertCell(3);
+  c4.innerHTML = mean;
+  let c5 = row.insertCell(4);
+    c5.innerHTML = median;
+    let c6 = row.insertCell(5);
+    c6.innerHTML = mostCommon;
+  }
   c1.innerHTML = i;
   let count = 0;
   for (j = 0; j < finalResults.length; j++) {
@@ -141,3 +253,4 @@ if (numDice == 3) {
 }
 
 }
+
